b464800c936702deafb1c9a85fcb55c60a45aef5
ellapresso <ellapresso@gmail.com>
2019-04-17 11:53:57 +0900

    kakao

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ const Router = require('koa-router');
 const testCtrl = require('./controllers/test2');
 const postCtrl = require('./controllers/post');
 const hashCtrl = require('./controllers/hash');
+const loginCtrl = require('./controllers/login');
 
 const POST = '/api/post';
 const HASH = '/api/hash';
