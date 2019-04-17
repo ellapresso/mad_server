18f8ab299507b67f9be8ee6d2e104f912ee52693
ellapresso <ellapresso@gmail.com>
2019-04-17 18:45:33 +0900

    카카오 유저 정보 요청

--- server/api/controllers/login.js
+++ server/api/controllers/login.js
@@ -1,21 +1,18 @@
 'use strict';
-// const axios = require(axios);
+const axios = require('axios');
 // const POST = require('../models/post');
 // 글 목록
 const kakaoLogin = async (ctx) => {
-    const req = ctx.request.body;
-    // axios({
-    //     method: 'post',
-    //     url: 'https://kapi.kakao.com/v2/user/me',
-    //     headers: 'Authorization: Bearer ric62_woWy7UMpAV-fRik2TNgWOIijdE7l-FYAopyWAAAAFqKkiLKQ',
-    // }).then(function (response) {
-    //     console.log(response);
-    //     return ctx.send(200, response);
-    // });
-    return ctx.send(200, {
-        req_body: req.access_token,
-        ctx,
-    });
+    const token = ctx.request.body;
+    return axios({
+        method : 'post',
+        url : 'https://kapi.kakao.com/v2/user/me',
+        headers : token,
+        responseType : 'json',
+    }).then(function(response){
+        ctx.send(200,response.data);
+    })
+
 };
 
 module.exports.kakaoLogin = kakaoLogin;
