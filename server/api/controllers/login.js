83faaa771adb158edfe368d34e607451c78889ef
ellapresso <ellapresso@gmail.com>
2019-04-17 13:40:48 +0900

    test

--- server/api/controllers/login.js
+++ server/api/controllers/login.js
@@ -4,9 +4,9 @@
 // 글 목록
 const kakaoLogin = async (ctx) => {
     const req = ctx.request.body;
-    console.log(ctx);
     return ctx.send(200, {
         req_body: req,
+        params: ctx.params,
         ctx,
     });
 };
