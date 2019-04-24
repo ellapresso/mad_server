458e83dedd86e33e55ef270b26459a39c1f1c400
ellapresso <ellapresso@gmail.com>
2019-04-24 13:34:47 +0900

    글 순서

--- server/api/models/post.js
+++ server/api/models/post.js
@@ -7,7 +7,7 @@ const Post = {
     getPost: () => {
         return madDatabase
             .promise()
-            .query('select `posts`.`pno`,`writer`,`title`,`contents`,`hashes`,`likes`.`lCount` as likes,`wrDate`,`upDate` from `posts` join(select `pno`, GROUP_CONCAT(`hContent` SEPARATOR ",") as `hashes` from `hashes` group by `pno`) `hash` on `posts`.`pno` = `hash`.`pno` left join `likes` on `posts`.`pno` = `likes`.`pno`')
+            .query('select `posts`.`pno`,`writer`,`title`,`contents`,`hashes`,`likes`.`lCount` as likes,`wrDate`,`upDate` from `posts` join(select `pno`, GROUP_CONCAT(`hContent` SEPARATOR ",") as `hashes` from `hashes` group by `pno`) `hash` on `posts`.`pno` = `hash`.`pno` left join `likes` on `posts`.`pno` = `likes`.`pno` order by `posts`.`pno` desc')
             .then(([rows]) => {
                 return rows;
             });
