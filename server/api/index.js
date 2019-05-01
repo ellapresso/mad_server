cb766d2747e64bbf29c8a0b5e4a6739e86ff7e9c
ellapresso <ellapresso@gmail.com>
2019-05-01 23:49:47 +0900

    test2 삭제

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,6 @@
 
 const Router = require('koa-router');
 
-const testCtrl = require('./controllers/test2');
 const postCtrl = require('./controllers/post');
 const hashCtrl = require('./controllers/hash');
 const likeCtrl = require('./controllers/like');
@@ -16,8 +15,6 @@ module.exports = (app) => {
 
     app.use(router.routes());
     app.use(router.allowedMethods());
-
-    router.get(`/api/test2`, testCtrl.getTest);
 
     router.post(`/kakaologin`, loginCtrl.kakaoLogin);
 
