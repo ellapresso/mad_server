cc9ccc6a270abc6bdc15c24214961596cf8229b8
ellapresso <ellapresso@gmail.com>
2019-05-20 17:08:57 +0900

    유저정보

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -4,8 +4,7 @@ const USER = require('../models/user');
 
 const getUserInfo = async (ctx) => {
     const userId = ctx.request.body.userId;
-
-    const userInfo = await getUserInfo(userId);
+    const userInfo = await USER.getUserInfo(userId);
 
     return ctx.send(200, {
         userInfo,
